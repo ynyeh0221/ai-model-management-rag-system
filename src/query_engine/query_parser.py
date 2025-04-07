b52a6c9f18ccb1293e1ec023975d04e71c9d11b5
Yi-Nung Yeh <ynyeh0221@gmail.com>
2025-04-07 12:49:55 -0700

    Create query_parser.py

---
+++ src/query_engine/query_parser.py
@@ -0,0 +1,20 @@
+# src/query_engine/query_parser.py
+class QueryParser:
+    def __init__(self):
+        pass
+    
+    def parse_query(self, query_text):
+        """Parse a query to determine intent and parameters."""
+        pass
+    
+    def classify_intent(self, query_text):
+        """Classify the intent of a query."""
+        pass
+    
+    def extract_parameters(self, query_text):
+        """Extract parameters from a query."""
+        pass
+    
+    def preprocess_query(self, query_text):
+        """Preprocess a query for searching."""
+        pass
