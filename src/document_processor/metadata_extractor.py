e9422c32bd1cb6c1bd8ad75c28fbc6314c8a529c
Yi-Nung Yeh <ynyeh0221@gmail.com>
2025-04-07 12:46:17 -0700

    Create metadata_extractor.py

---
+++ src/document_processor/metadata_extractor.py
@@ -0,0 +1,20 @@
+# src/document_processor/metadata_extractor.py
+class MetadataExtractor:
+    def __init__(self):
+        pass
+    
+    def extract_metadata(self, file_path):
+        """Extract metadata from a file."""
+        pass
+    
+    def extract_git_metadata(self, file_path):
+        """Extract Git metadata for the file."""
+        pass
+    
+    def extract_file_metadata(self, file_path):
+        """Extract basic file metadata."""
+        pass
+    
+    def find_associated_config(self, file_path):
+        """Find associated configuration files."""
+        pass
